428ba00a44699d66f91a3834c52f58035ab38959
Ivan GaLa <github@laodesign.es>
2024-10-16 13:46:26 +0200

    Voy completando el modelo.

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -6,12 +6,34 @@ export class Tasks {
     this.table = configData.tables.tasks.name
     this.fields = configData.tables.tasks.fields
   }
+
+
+  // Devuelve todas las tareas
   async getAll() {
     const sql = `SELECT * FROM ${this.table}`
     const response = await dbCon.execute(sql);
     return response.rows;
   }
 
+
+  // Devuelve todas las tareas de un usuario
+  async getAllByUserId(userid) {
+    const sql = `SELECT * FROM ${this.table} WHERE user=${userid};`
+    const response = await dbCon.execute(sql);
+    return response.rows;
+  }
+
+
+  // Devuelve una tarea según su id
+  async getTaskById(id) {
+    const sql = `SELECT * FROM ${this.table} WHERE id=${id};`
+    const response = await dbCon.execute(sql);
+    // Devuelve directamente la tarea
+    return response.rows[0];
+  }
+
+
+  // Añade una tarea
   async add(data) {
     // Obtengo un array solo con los campos del formulario que no son null y existen en el modelo Tasks
     const fieldsToAdd = Object.values(data)
@@ -33,6 +55,9 @@ export class Tasks {
 
     const sql = `INSERT INTO "${this.table}" ("${fields}") VALUES ("${values}");`
 
-    await dbCon.execute(sql)
+    const response = await dbCon.execute(sql)
+
+    // Devolvemos el objeto response completo ya que en el insert no hay rows como tal.
+    return response;
   }
 }
