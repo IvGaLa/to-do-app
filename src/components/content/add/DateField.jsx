c11ebebd948fb35c2f90c508db0a0a6ef5733147
Ivan GaLa <github@laodesign.es>
2024-10-16 17:22:55 +0200

    Añado etiqueta para que muestre el mensaje de error de las validaciones.

--- src/components/content/add/DateField.jsx
+++ src/components/content/add/DateField.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function DateField({ name, handlerChange, formData }) {
+function DateField({ name, handlerChange, formData, validationErrors }) {
   const [dateAt, setDateAt] = useState("");
 
   const handlerDate = (date) => {
@@ -20,6 +20,7 @@ function DateField({ name, handlerChange, formData }) {
         value={dateAt}
         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
       />
+      {validationErrors[name] && <span>{validationErrors[name]}</span>}
     </div>
   );
 }
