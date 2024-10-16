e0bf9acaccc88b0dac1c0cacd48042044a87dbbe
Ivan GaLa <github@laodesign.es>
2024-10-16 23:10:35 +0200

    Hago un componente para los enlaces de la cabecera.

---
+++ src/components/header/LinkHeader.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function LinkHeader({ children, to }) {
+  return (
+    <Link
+      to={to}
+      className="inline-block px-4 hover:text-textsecondary duration-200"
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default LinkHeader;
