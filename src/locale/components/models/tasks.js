daedfb65cec0dc557fa1bc74a057f8e5eec22f0b
Ivan GaLa <github@laodesign.es>
2024-10-17 20:40:18 +0200

    Añado un locale para los labels de los campos del modelo tasks.

---
+++ src/locale/components/models/tasks.js
@@ -0,0 +1,12 @@
+export const tasks = {
+  label: {
+    id: 'Id',
+    user: 'Usuario',
+    title: 'Título',
+    description: 'Descripción',
+    finished: 'Finalizada',
+    createdAt: 'Creada',
+    modifiedAt: 'Modificada',
+    finishedAt: 'Finalizada',
+  }
+}
