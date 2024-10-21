54f60707029b9bf6fff3ab04951e0cb7899a2aed
Ivan GaLa <github@laodesign.es>
2024-10-21 22:55:54 +0200

    Modifico el "tooltip" para mostrar los errores de validación del formulario.

--- src/components/content/add/ErrorMessage.jsx
+++ src/components/content/add/ErrorMessage.jsx
@@ -1,5 +1,16 @@
 function ErrorMessage({ error }) {
-  return <>{error && <p className="text-red-400 ">{error}</p>}</>;
+  return (
+    <>
+      {error && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-1"
+          role="alert"
+        >
+          <span className="block sm:inline">{error}</span>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ErrorMessage;
