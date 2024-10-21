0d38f4eac4e8021c5ff4ea73f6db243226154a30
Ivan GaLa <github@laodesign.es>
2024-10-21 13:01:20 +0200

    Añado algunos alias en vite.config.js para utilizarlos en los imports.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+      '@locales': fileURLToPath(new URL('./src/locales', import.meta.url)),
+      '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+      '@validations': fileURLToPath(new URL('./src/validations', import.meta.url)),
+    },
+  },
 })
