d723d7db1176a4e5899d7f9bd76538ceccd3636a
Ivan GaLa <github@laodesign.es>
2024-10-28 20:10:34 +0100

    Restauro componente eliminado por error.

---
+++ src/components/content/modify/Updated.jsx
@@ -0,0 +1,18 @@
+/**
+ *
+ * Componente para mostrar un mensaje de ok al modificar una tarea
+ *
+ */
+
+import OkMessage from "@components/form/OkMessage";
+import { getLocale } from "@locales/es";
+
+function Updated() {
+  return (
+    <div>
+      <OkMessage ok={getLocale("components.content.modify.taskUpdated")} />
+    </div>
+  );
+}
+
+export default Updated;
