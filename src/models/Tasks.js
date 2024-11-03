88e4b81977e24db68f2be68b2077b7cb74b084a1
Ivan GaLa <github@laodesign.es>
2024-11-03 12:08:29 +0100

    Refactorizo el modelo Tasks y el ToDoApp para que gestione el nombre de la tabla y los campos el propio modelo Tasks en lugar del genérico ToDoApp

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -1,43 +1,67 @@
+/**
+ * 
+ * Modelo para la tabla tasks
+ * 
+ * Aquí solo definimos los métodos a utilizar, es un bypass hacia el modelo genérico al que llamo el mismo método con la tabla del modelo
+ * 
+ */
+
+import { configData } from "@config/config";
 import { ToDoApp } from "@models/ToDoApp";
 
 export class Tasks {
 
+  // Definimos un objeto tabla que contendra el nombre y los campos
+  static table = {}
+
+  static getTableName() {
+    this.table.name = configData.tables['tasks'].name
+    this.table.fields = configData.tables[this.table.name].fields
+  }
+
   // Devuelve todas las tareas
   static async getAll() {
+    this.getTableName()
     return ToDoApp.getAll(this.table)
   }
 
 
   // Devuelve todas las tareas de un usuario
   static async getAllByUserId(userid) {
-    return ToDoApp.getAllByUserId(userid)
+    this.getTableName()
+    return ToDoApp.getAllByUserId(userid, this.table)
   }
 
 
   // Devuelve una tarea según su id
   static async getById(id) {
-    return ToDoApp.getById(id)
+    this.getTableName()
+    return ToDoApp.getById(id, this.table)
   }
 
 
   // Añade una tarea
   static async add(data) {
-    return ToDoApp.add(data)
+    this.getTableName()
+    return ToDoApp.add(data, this.table)
   }
 
 
   // Elimina una tarea por el id
   static async deleteById(id) {
-    return ToDoApp.deleteById(id)
+    this.getTableName()
+    return ToDoApp.deleteById(id, this.table)
   }
 
   // Actualiza la fecha en finishedAt a la fecha actual o a null dependiende de state
   static async setToggle(id, state) {
-    return ToDoApp.setToggle(id, state)
+    this.getTableName()
+    return ToDoApp.setToggle(id, state, this.table)
   }
 
   // Hacemos un update de la tarea.
   static async update(data) {
-    return ToDoApp.update(data)
+    this.getTableName()
+    return ToDoApp.update(data, this.table)
   }
 }
--- src/models/ToDoApp.js
+++ src/models/ToDoApp.js
@@ -1,41 +1,29 @@
-import { configData } from "@config/config";
 import { dbCon } from "@config/dbConnect";
 import { sanitizeInput } from "@validations/sanitize";
 import { getTodayDate } from "@lib/datetime";
 import { getLocale } from "@locales/es";
 
 export class ToDoApp {
-  tablename = ''
-  fields = {}
-
-  // Establece el nombre de la tabla del modelo que lo ha llamado y sus correspondientes campos.
-  static getTableName(table = 'tasks') {
-    this.tablename = configData.tables[table].name
-    this.fields = configData.tables[table].fields
-  }
 
   // Devuelve todas las tareas
-  static async getAll(table = 'tasks') {
-    this.getTableName(table)
-    const sql = `SELECT * FROM ${this.tablename}`
+  static async getAll(table) {
+    const sql = `SELECT * FROM ${table.name}`
     const response = await dbCon.execute(sql);
     return response.rows;
   }
 
 
   // Devuelve todas las tareas de un usuario
-  static async getAllByUserId(userid, table = 'tasks') {
-    this.getTableName(table)
-    const sql = `SELECT * FROM ${this.tablename} WHERE user='${sanitizeInput(userid)}';`
+  static async getAllByUserId(userid, table) {
+    const sql = `SELECT * FROM ${table.name} WHERE user='${sanitizeInput(userid)}';`
     const response = await dbCon.execute(sql);
     return response.rows;
   }
 
 
   // Devuelve una tarea según su id
-  static async getById(id, table = 'tasks') {
-    this.getTableName(table)
-    const sql = `SELECT * FROM ${this.tablename} WHERE id=${parseInt(sanitizeInput(id))};`
+  static async getById(id, table) {
+    const sql = `SELECT * FROM ${table.name} WHERE id=${parseInt(sanitizeInput(id))};`
     const response = await dbCon.execute(sql);
     // Devuelve directamente la tarea
     return response.rows[0];
@@ -43,12 +31,11 @@ export class ToDoApp {
 
 
   // Añade una tarea
-  static async add(data, table = 'tasks') {
-    this.getTableName(table)
+  static async add(data, table) {
     // Obtengo un array solo con los campos del formulario que no son null y existen en el modelo Tasks
-    const { fields, values } = this.getValidFieldsToAdd(data)
+    const { fields, values } = this.getValidFieldsToAdd(data, table)
 
-    const sql = `INSERT INTO "${this.tablename}" ("${fields}") VALUES ("${values}");`
+    const sql = `INSERT INTO "${table.name}" ("${fields}") VALUES ("${values}");`
 
     const response = await dbCon.execute(sql)
 
@@ -59,9 +46,8 @@ export class ToDoApp {
 
 
   // Elimina una tarea por el id
-  static async deleteById(id, table = 'tasks') {
-    this.getTableName(table)
-    const sql = `DELETE FROM ${this.tablename} WHERE id=${sanitizeInput(id)};`
+  static async deleteById(id, table) {
+    const sql = `DELETE FROM ${table.name} WHERE id=${sanitizeInput(id)};`
     const response = await dbCon.execute(sql)
     return response;
   }
@@ -69,12 +55,11 @@ export class ToDoApp {
 
 
   // Actualiza la fecha en finishedAt a la fecha actual o a null dependiende de state
-  static async setToggle(id, state, table = 'tasks') {
-    this.getTableName(table)
+  static async setToggle(id, state, table) {
 
     const finishedAt = (state === 1) ? `"${getTodayDate(getLocale("formatdatetimetodb"))}"` : 'NULL'
 
-    const sql = `UPDATE "${this.tablename}" SET finishedAt=${finishedAt} WHERE id = ${sanitizeInput(id.value)}`
+    const sql = `UPDATE "${table.name}" SET finishedAt=${finishedAt} WHERE id = ${sanitizeInput(id.value)}`
 
     const response = await dbCon.execute(sql)
 
@@ -83,11 +68,10 @@ export class ToDoApp {
   }
 
   // Actualizo una tarea
-  static async update(data, table = 'tasks') {
-    this.getTableName(table)
+  static async update(data, table) {
     // Obtengo un array solo con los campos del formulario que no son null y existen en el modelo Tasks
-    const valuesToUpdate = this.getValidFieldsToUpdate(data)
-    const sql = `UPDATE "${this.tablename}" SET ${valuesToUpdate} WHERE id = ${data.id.value};`
+    const valuesToUpdate = this.getValidFieldsToUpdate(data, table)
+    const sql = `UPDATE "${table.name}" SET ${valuesToUpdate} WHERE id = ${data.id.value};`
     const response = await dbCon.execute(sql)
 
     // Devolvemos el objeto response completo ya que en el insert no hay rows como tal.
@@ -95,10 +79,9 @@ export class ToDoApp {
   }
 
   // Obtengo un array solo con los campos del formulario que no son null y existen en el modelo Tasks
-  static getValidFields(data, table = 'tasks') {
-    this.getTableName(table)
+  static getValidFields(data, table) {
     return Object.values(data)
-      .filter(field => field.value !== '' && field.value !== null && this.fields[field.name])
+      .filter(field => field.value !== '' && field.value !== null && table.fields[field.name])
       .map(field => ({
         name: field.name,
         value: field.value
@@ -106,9 +89,8 @@ export class ToDoApp {
   }
 
   // Obtengo los datos para hacer el update
-  static getValidFieldsToUpdate(data, table = 'tasks') {
-    this.getTableName(table)
-    const validFields = this.getValidFields(data)
+  static getValidFieldsToUpdate(data, table) {
+    const validFields = this.getValidFields(data, table)
     return validFields.map((field) => {
       const fieldValue = Number.isInteger(field.value) ? field.value : `'${field.value}'`
       return `"${field.name}" = ${fieldValue}`
@@ -117,9 +99,8 @@ export class ToDoApp {
 
 
   // Obtengo los fields y los values por separado separados por comas para el insert.
-  static getValidFieldsToAdd(data, table = 'tasks') {
-    this.getTableName(table)
-    const validFields = this.getValidFields(data)
+  static getValidFieldsToAdd(data, table) {
+    const validFields = this.getValidFields(data, table)
 
     const fields = validFields.map((field) => {
       return field.name
